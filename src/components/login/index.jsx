1c38294587c2c7c5029e10e740cf5f0f2ea1fa2c
fusheng <2370772476@qq.com>
2020-01-22 11:05:30 +0800

    完成login表单校验功能

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import { Form, Icon, Input, Button} from 'antd';
 
+//图片需要引入才可以使用
 import logo from './logo.png';
 import './index.less';
-export default class Login extends Component {
-  render() {
+@Form.create()
+class Login extends Component {
+
+  
+  //自定义表单校验
+  validator = (rule,value,callback)=>{
+    //console.log(rule,value);
+    //console.log(rule.field);
+    const reg = /^\w+$/;
+    const name = rule.field === 'username'?'用户名':'密码';
+    if(!value){
+      callback(`请输入您的${name}`);
+    }else if(value.length < 5){
+      callback(`${name}长度最短为5个字符`);
+    }else if(value.length > 15){
+      callback(`${name}长度不能超过为15个字符`);
+    }else if(!reg.test(value)){//检测value的值
+      callback(`${name}只能是字母、数字、下划线`);
+    }
+    //不管怎么样都会触发回调函数
+    callback();
+  }
+    render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className='login'>
         <header className='login-header'>
@@ -15,20 +38,47 @@ export default class Login extends Component {
           <h3>用户登录</h3>
           <Form className='login-form '>
             <Form.Item>
-            <Input
+            {
+              getFieldDecorator(
+                'username',
+                {
+                  rules:[
+                    {
+                      validator:this.validator
+                    }
+                  ]
+                }
+              )(
+                <Input
                   prefix={
                     <Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />
                   }
                   placeholder='密码'
                 />
+              )
+            }
             </Form.Item>
             <Form.Item>
-            <Input
+              {
+                getFieldDecorator(
+                  'password',
+                  {
+                    rules:[
+                      {
+                        validator:this.validator
+                      }
+                    ]
+                  }
+                
+                )(
+                  <Input
                   prefix={
                     <Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />
                   }
                   placeholder='密码'
                 />
+                )
+              }
             </Form.Item>
             <Form.Item>
               <Button type="primary" className='login-form-btn'>登录</Button>
@@ -39,3 +89,5 @@ export default class Login extends Component {
     )
   }
 }
+//暴露
+export default Login;
